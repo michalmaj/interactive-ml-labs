41b6e30afc837f665dd126ff5abc3ec920e1d9ee
Michał Maj <michalpmaj@gmail.com>
2026-05-12 10:51:32 +0200

    feat(boosting): add synthetic weighted dataset

--- demos/level_2_practical_ml/02_boosting_mistake_lab/src/boosting_mistake_lab/__init__.py
+++ demos/level_2_practical_ml/02_boosting_mistake_lab/src/boosting_mistake_lab/__init__.py
@@ -1,3 +1,21 @@
 """Boosting Mistake Lab demo package."""
 
-__all__: list[str] = []
+from boosting_mistake_lab.dataset import (
+    DATASET_KIND_AXIS_ALIGNED,
+    DATASET_KIND_XOR,
+    SyntheticWeightedDatasetConfig,
+    WeightedDatasetSplit,
+    WeightedTrainTestDataset,
+    make_synthetic_weighted_dataset,
+    make_uniform_sample_weights,
+)
+
+__all__ = [
+    "DATASET_KIND_AXIS_ALIGNED",
+    "DATASET_KIND_XOR",
+    "SyntheticWeightedDatasetConfig",
+    "WeightedDatasetSplit",
+    "WeightedTrainTestDataset",
+    "make_synthetic_weighted_dataset",
+    "make_uniform_sample_weights",
+]
